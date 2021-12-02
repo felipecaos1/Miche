4510e04bf5985c850c7e5915722a2ddf1378a6cb
felipecaos1 <andresfelipecao@gmail.com>
2021-12-01 22:10:55 -0500

    login

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,14 @@ import { Fragment } from "react";
 function Login(){
     return (
         <Fragment>
-            <p>hola</p>
+            <div class="login">
+            <h1>Login</h1>
+            <form method="post">
+                <input type="text" name="u" placeholder="Username" required="required" />
+                <input type="password" name="p" placeholder="Password" required="required" />
+                <button type="submit" class="btn btn-primary btn-block btn-large">Ingresar</button>
+            </form>
+        </div>
         </Fragment>
     )
 }
