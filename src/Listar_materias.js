4b9b9c14801d35d7105ea7c9f0b7b7180cc7d2e9
felipecaos1 <andresfelipecao@gmail.com>
2021-12-18 11:23:57 -0500

    editar materia prima

--- src/Listar_materias.js
+++ src/Listar_materias.js
@@ -2,13 +2,24 @@ import Menu_vertical from './Menu_vertical';
 import Header from './Header';
 import Footer from './Footer';
 import Ir_atras from './Ir_atras';
-import {useState } from 'react';
+import {useState, useRef } from 'react';
 import React from 'react';
 
  
 function Listar_materias() {
   const [lista, setLista] = useState([]);
-  const [listaeditar, setListaeditar] = useState([]);
+
+
+  //referencia a las cajas de te texto para editar
+
+  var ref_nombre=useRef();
+  var ref_descripcion=useRef();
+  var ref_unidad=useRef();
+  var ref_disponibilidad=useRef();
+  var ref_precio=useRef();
+///////////////////////////////////////////////
+
+
   React.useEffect(() => {
     actualizar_lista();
   }, [])
@@ -25,14 +36,36 @@ function Listar_materias() {
       .catch(err => alert(err))
     setLista(temp);
   }
-
+  const ocultar=()=>{
+    var x= document.getElementById("prueba");
+    x.style.display="none";
+  }
   const editar=(id)=>{
    // const lista_materias_primas = { "id": "5353", "nombre": "tipo1", "descripcion": "cafe prensada", "unidad": "m2", "cantidad": "56", "precio": "29.000" };
-    
+    console.log(id);
+    var x= document.getElementById("prueba");
+    x.style.display="block";
     const lista_materias_primas=lista.find(item=>item._id===id);
-    const lista2=[lista_materias_primas];
-    setListaeditar(lista2);
     
+    ref_nombre.current.value=lista_materias_primas.nombre;
+    ref_descripcion.current.value=lista_materias_primas.descripcion;
+    ref_unidad.current.value=lista_materias_primas.unidad;
+    ref_disponibilidad.current.value=lista_materias_primas.cantidad;
+    ref_precio.current.value=lista_materias_primas.precio;  
+    
+  }
+
+  const actualizar_mp=(id2)=>{
+    //recojemos los datos de los input de editar
+    const editar_nombre=ref_nombre.current.value;
+    const editar_descripcion=ref_descripcion.current.value;
+    const editar_unidad=ref_unidad.current.value;
+    const editar_disponibilidad=ref_disponibilidad.current.value;
+    const editar_precio=ref_precio.current.value;
+
+    ///consultar a la base de datos con el nombre y actualizar 
+    
+  
   }
 
   return (
@@ -47,73 +80,72 @@ function Listar_materias() {
           {/*<!-- End Navbar -->*/}
           <div className="content">
             <Ir_atras />
+            
+            <div id="prueba" className="row"  style={{display:"none"}}>
+              <div className="col-md-12">
+                <div className="card">
+                  <h3 style={{margin:"10px"}}>Editar Materia Prima </h3>
+                <table className="table">
+                    <thead className=" text-primary">
+                      
+                      <th>
+                        Nombre
+                      </th>
+                      <th>
+                        Descripcion
+                      </th>
+                      <th>
+                        Unidad
+                      </th>
+                      <th >
+                        Disponibilidad
+                      </th>
+                      <th >
+                        Precio
+                      </th>
+                     
+                    </thead>
+                    <tbody>
+                      
+                           <tr>
+                             
+                             <td>
+                                <input ref={ref_nombre}/>
+                             </td>
+                             <td>
+                             <input ref={ref_descripcion}/>
+                             </td>
+                             <td>
+                             <input ref={ref_unidad}/>
+                             </td>
+                             <td>
+                             <input ref={ref_disponibilidad}/>
+                             </td>
+                             <td >
+                             <input ref={ref_precio}/>
+                             </td>
+                             
+                           </tr>
 
-              <div className="row" id="modal-editar-mp" style={{}}>
-                  <div className="col-md-12">
-                    <div className="card">
-                    <table className="table">
-                        <thead className=" text-primary">
-                          
-                          <th>
-                            Nombre
-                          </th>
-                          <th>
-                            Descripcion
-                          </th>
-                          <th>
-                            Unidad
-                          </th>
-                          <th >
-                            Disponibilidad
-                          </th>
-                          <th >
-                            Precio
-                          </th>
-                         
-                        </thead>
-                        <tbody>
-                          {
-                             listaeditar.map(item =>
-                               <tr>
-                                 
-                                 <td>
-                                    <input placeholder={item.nombre}></input>
-                                 </td>
-                                 <td>
-                                 <input placeholder={item.descripcion}></input>
-                                 </td>
-                                 <td>
-                                 <input placeholder={item.unidad}></input>
-                                 </td>
-                                 <td>
-                                 <input placeholder={item.cantidad}></input>
-                                 </td>
-                                 <td >
-                                 <input placeholder={item.precio}></input>
-                                 </td>
-                                 
-                               </tr>
-
-                             )
-                          }
-
-
-                        </tbody>
-                        <tfoot>
-                          <tr>
-                            
-                            <td style={{ textAlign: "center" }}>
-                                   <a href="#" > Actualizar </a> 
-                            </td>
-                            <td style={{ textAlign: "center" }}>
-                                   <a href="#" > Cancelar </a>
-                            </td>
-                          </tr>
-                        </tfoot>
-                      </table>
-                    </div>
-                  </div>
+                    </tbody>
+                    <tfoot>
+                      <tr>
+                        
+                        <td style={{ textAlign: "center" }}>
+                               <a href="#" onClick={()=>actualizar_mp()}> Actualizar </a> 
+                        </td>
+                        <td style={{ textAlign: "center" }}>
+                               <a href="#" onClick={ocultar} > Cancelar </a>
+                        </td>
+                      </tr>
+                    </tfoot>
+                  </table>
+                </div>
               </div>
+          </div>
+            
+
+              
 
             <div className="row">
               <div className="col-md-12">
